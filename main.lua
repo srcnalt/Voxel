1e9c31f64a5fde207d9df25a53fdbc6cd98a1096
Sercan Altundas <sercan.altundas@gmail.com>
2022-02-02 21:42:24 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,27 +19,28 @@ function love.load()
 
 	love.window.setMode(screen.w, screen.h, {fullscreen = false})
 
-	v1 = lg.newImage('images/b1.png')
-
 	l1 = {8,7,6,2,3,4,5,5,5,5,4,3,2,1}
-	l2 = {1,2,7,4,6,5,4,3,2,1}
-	l3 = {5,4,4,4,3,2,1}
+	l2 = {1,2,3,6,5,7,4,3,2,1}
+
+	v1 = lg.newImage('images/b1.png')
+	v2 = lg.newImage('images/burger.png')
 
 	r = 0
 
 	vox1 = Voxel.new(v1, 14, l1)
+	vox2 = Voxel.new(v2, 12, l2)
 end
 
 function love.update(dt)
 	r = r + dt
 	vox1.rot = r
-	--vox2.rot = r
+	vox2.rot = r
 	--vox3.rot = r
 end
 
 function love.draw()
 	vox1:draw(100, 100)
-	--vox2:draw(300, 100)
+	vox2:draw(300, 100)
 	--vox3:draw(500, 100)
 
 	--love.graphics.rotate(r)
